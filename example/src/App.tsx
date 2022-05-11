8697bd57a4361b01bdc819be674adcba01225780
Chetan Godiya <chetan.g@simformsolutions.com>
2022-05-11 11:41:24 +0530

    feat(UNT-T8207): Setup base code structure

---
+++ example/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { CounterButton } from 'react-native-country-code-select'
+
+const App = () => {
+  return <CounterButton label={'Press Here'} />
+}
+
+export default App
