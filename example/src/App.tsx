73e87b6f22eb8a4b7f67c84d0fe88d6e87a34039
Mukesh Jangid <mukesh.j@simformsolutions.com>
2022-06-03 14:10:07 +0530

    fix(UNT-T8473): fixed example app UI issues

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { CountryPickerModal } from 'react-native-country-code-select';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -12,17 +12,16 @@ const App = () => {
         <Text style={styles.textView}>
           {JSON.stringify(selectedValue, null, 2)}
         </Text>
-        <Text onPress={() => setIsVisible(oldStatus => !oldStatus)}>
-          Open picker
-        </Text>
+        <Text style={styles.separator}>{'-- --'}</Text>
+        <TouchableOpacity 
+          activeOpacity={0.8} 
+          style={styles.selectCountryButton}
+          onPress={() => setIsVisible(oldStatus => !oldStatus)}
+        >
+          <Text style={styles.selectCountry}>Select Country</Text>
+        </TouchableOpacity>
         <CountryPickerModal
           isVisible={isVisible}
-          isFlagVisible
-          isAlphabetsVisible
-          isSearchInputVisible
-          isCloseButtonVisible
-          emptyText='no data'
-          headerSearchPlaceholder="Search here..."
           onClose={() => setIsVisible(false)}
           {...{onSelect: setSelectedValue}}
         />
@@ -34,12 +33,42 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: 'grey'},
-  textView: {width: '60%', backgroundColor: 'lightgrey', padding: 10},
+  container: {
+    flex: 1, 
+    backgroundColor: '#e6d7b8'
+  },
+  textView: {
+    width: '60%', 
+    backgroundColor: '#261a08', 
+    padding: 20, 
+    borderWidth: 1, 
+    borderRadius: 20, 
+    overflow: 'hidden', 
+    color: '#ffffff', 
+    fontWeight: '500'
+  },
   viewStyle: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
     flex: 1,
   },
+  selectCountryButton: {
+    width: '60%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 15,
+    backgroundColor: '#261a08',
+    borderRadius: 20,
+  },
+  selectCountry: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  separator: {
+    marginVertical: 20,
+    letterSpacing: 2,
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
 });
